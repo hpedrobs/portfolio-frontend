3f046c92af06199b96f7cb7d68fcc758dfeba822
Pedro Henrique <hpedrobs@gmail.com>
2024-09-20 02:45:38 -0300

    Mudar ícone Clock

--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -1,5 +1,5 @@
 import '../assets/project.css'
-import { ClockIcon } from '@heroicons/react/24/solid'
+import { FaClock } from "react-icons/fa"
 
 function Project() {
     return (
@@ -7,7 +7,7 @@ function Project() {
             <h2 className="h2 mb-10">Projetos</h2>
             <div className="flex items-center justify-center">
                 <div className="text-center">
-                    <ClockIcon className="h-14 w-14 text-yellow-500 mx-auto mb-4" />
+                    <FaClock className="h-14 w-14 text-yellow-500 mx-auto mb-4" />
                     <h1 className="text-2xl font-bold text-yellow-500">Em breve...</h1>
                 </div>
             </div>
