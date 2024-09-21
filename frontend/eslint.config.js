5bdd2c9eb8a8f08bf86d6903f8cc65facaefb9fa
Pedro Henrique <hpedrobs@gmail.com>
2024-09-21 18:28:46 -0300

    Indentação para 4 espaços e nunca usar ponto e vírgula

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,24 +5,26 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        },
+        plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        },
+        rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'indent': ['error', 4], // Indentação de 4 espaços
+        'semi': ['error', 'never'], // Sem ponto e vírgula
+        }
+    }
 )
