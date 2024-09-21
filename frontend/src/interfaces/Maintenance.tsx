23faa99573126bcadca9cb154d80105b19a5251b
Pedro Henrique <hpedrobs@gmail.com>
2024-09-21 18:29:28 -0300

    Criar página de manutenção e colocar o site em manutenção

---
+++ frontend/src/interfaces/Maintenance.tsx
@@ -0,0 +1,13 @@
+import { FaTools } from 'react-icons/fa'
+
+const Maintenance = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <FaTools className="text-green-500 text-6xl mb-4 animate-pulse" />
+            <h1 className="text-4xl font-bold text-gray-800">O site está em manutenção!</h1>
+            <p className="text-lg text-gray-600 mt-2">Estamos trabalhando para trazer melhorias em breve.</p>
+        </div>
+    )
+}
+
+export default Maintenance
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// import App from './interfaces/App'
+import UnderConstruction from './interfaces/Maintenance'
 import './assets/index.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        {/* <App /> */}
+        <UnderConstruction />
     </StrictMode>
 )
