59aba3acc8ee5c64efde34b724e7b5478f69e689
Pedro Henrique <hpedrobs@gmail.com>
2024-10-15 11:05:23 -0300

    Mudar o ícone, colocar um enfeito pulsante e mudar a cor

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,5 @@
 import '../assets/project.css'
-import { FaClock } from "react-icons/fa"
+import { FaTools } from "react-icons/fa"
 
 function Project() {
     return (
@@ -7,8 +7,8 @@ function Project() {
             <h2 className="h2 mb-10">Projetos</h2>
             <div className="flex items-center justify-center">
                 <div className="text-center">
-                    <FaClock className="h-14 w-14 text-yellow-500 mx-auto mb-4" />
-                    <h1 className="text-2xl font-bold text-yellow-500">Em breve...</h1>
+                    <FaTools className="text-yellow-500 text-6xl mb-4 animate-pulse mx-auto" />
+                    <h1 className="text-2xl font-bold text-gray-600">Em breve...</h1>
                 </div>
             </div>
         </section>
