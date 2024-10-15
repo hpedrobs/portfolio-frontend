52d97e997ede26721f62f47e5e4bac77bc1f1133
Pedro Henrique <hpedrobs@gmail.com>
2024-10-15 14:01:27 -0300

    Colocar distância entre os títulos

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
             <div className="flex flex-col lg:flex-row justify-center items-center mx-auto">
                 <article className="navbar-text-js lg:mb-0 px-5 text-center lg:text-start">
                     <h2>Developer Full-Stack</h2>
-                    <h1>Pedro Henrique</h1>
+                    <h1 className='my-4'>Pedro Henrique</h1>
                     <p>Sou Pedro Henrique, tenho 22 anos e sou de Goiânia, Goiás. Atualmente, trabalho como engenheiro de software, desenvolvendo soluções com muita paixão. Fique à vontade para explorar o portfólio que fiz com muito carinho. Espero que você curta a navegação!</p>
                 </article>
 
