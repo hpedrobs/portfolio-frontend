907877393ca1cd5631ef11230ca617f2d3df3ee1
Pedro Henrique <hpedrobs@gmail.com>
2024-10-18 13:45:36 -0300

    Retirar meu nome

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
                 <article className="navbar-text-js lg:mb-0 px-5 text-center lg:text-start">
                     <h2>Developer Full-Stack</h2>
                     <h1 className='my-4'>Pedro Henrique</h1>
-                    <p>Meu nome é Pedro Henrique, sou desenvolvedor de software em Goiânia, Goiás, com 6 anos de experiência no desenvolvimento de soluções voltadas para a otimização de processos e inovação. Meu portfólio reflete o compromisso com a qualidade e a eficiência em cada projeto. Sinta-se à vontade para explorá-lo.</p>
+                    <p>Sou desenvolvedor de software em Goiânia, Goiás, com 6 anos de experiência no desenvolvimento de soluções voltadas para a otimização de processos e inovação. Meu portfólio reflete o compromisso com a qualidade e a eficiência em cada projeto. Sinta-se à vontade para explorá-lo.</p>
                 </article>
 
                 <img src="./me.png" alt="Minha imagem ilustrada!" width={300} height={300}/>
