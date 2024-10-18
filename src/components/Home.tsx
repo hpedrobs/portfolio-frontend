e7afc3a25d2e63af69c7bba2661aedca055c6dea
Pedro Henrique <hpedrobs@gmail.com>
2024-10-18 13:43:35 -0300

    Mudar o texto de apresentação inicial

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
                 <article className="navbar-text-js lg:mb-0 px-5 text-center lg:text-start">
                     <h2>Developer Full-Stack</h2>
                     <h1 className='my-4'>Pedro Henrique</h1>
-                    <p>Sou Pedro Henrique, tenho 22 anos e sou de Goiânia, Goiás. Atualmente, trabalho como engenheiro de software, desenvolvendo soluções com muita paixão. Fique à vontade para explorar o portfólio que fiz com muito carinho. Espero que você curta a navegação!</p>
+                    <p>Meu nome é Pedro Henrique, sou desenvolvedor de software em Goiânia, Goiás, com 6 anos de experiência no desenvolvimento de soluções voltadas para a otimização de processos e inovação. Meu portfólio reflete o compromisso com a qualidade e a eficiência em cada projeto. Sinta-se à vontade para explorá-lo.</p>
                 </article>
 
                 <img src="./me.png" alt="Minha imagem ilustrada!" width={300} height={300}/>
