9005db048d21ee24f5170e47875795058a092ffd
Pedro Santos <pedro.santos@Facilita.local>
2025-04-03 14:07:20 -0300

    fix: retirar site da manutenção.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './interfaces/App'
+import App from './interfaces/App'
 import UnderConstruction from './interfaces/Maintenance'
 import './assets/index.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        {/* <App /> */}
-        <UnderConstruction />
+        <App />
+        {/* <UnderConstruction /> */}
     </StrictMode>
 )
